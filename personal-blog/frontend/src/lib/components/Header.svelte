934f0883f002b36a16d5096efa836fa9a1472422
InCogNiTo124 <msmetko@msmetko.xyz>
2025-12-30 23:56:07 +0100

    feat(blog): add a quote for my blog
    I came up with it some time ago, decided to add it. I had some more
    quotes I'd like to put, so in future there may be some animation for
    those (once i remember what they were)

--- personal-blog/frontend/src/lib/components/Header.svelte
+++ personal-blog/frontend/src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <div>
   <a href="/">msmetko's blog</a>
+  <p class="subtitle">A healthy dose of Dunning-Kruger</p>
 </div>
 
 <style scoped lang="css">
@@ -15,6 +16,15 @@
     font-size: 2.5rem;
     font-weight: 600;
     text-align: center;
+  }
+
+  .subtitle {
+    font-size: 1rem;
+    font-weight: 400;
+    margin: 0;
+    margin-top: 0.25rem;
+    font-style: italic;
+    opacity: 0.8;
   }
 
   @media screen and (min-width: 650px) {
